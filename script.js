6a4dceddb49cada6bc3953c1a5d9bf260e3b5854
meir biton <meirbit32@gmail.com>
2024-08-25 14:24:07 +0300

    fix nav bar >764px

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
             $(window).on("resize", function(){
                 if($(window).width() >= 764) {
                     $(".nav-list").css("display", "flex")
-                } 
+                } else{
+                    $(".nav-list").css("display", "none")
+                }
             })
             $(".material-symbols-outlined").on("click", function(event){
                 event.preventDefault()
