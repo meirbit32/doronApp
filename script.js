68e23000a4f2be546109853cc2787a19ca1d60b1
meir biton <meirbit32@gmail.com>
2024-09-11 00:04:06 +0300

    add read more button using jquery

--- script.js
+++ script.js
@@ -11,6 +11,29 @@
                 event.preventDefault()
                 $(".nav-list-toogle").toggle();
             })
+            var maxLength = 50;
+            $(".read-more-content").each(function(){
+                var myStr = $(this).text();
+                if($.trim(myStr).length > maxLength) {
+                    var newStr = myStr.substring(0, maxLength)
+                    var removedStr = myStr.substring(maxLength, $.trim(myStr).length)
+                    $(this).empty().html(newStr + '...');
+                    $(this).append('<span class=more-text>' + removedStr + '</span');
+                    $(this).append('<a href"=javascript:void(0);" class="read-more">Read More</a>');
+                }
+            })
+
+            $(".read-more").click(function(){
+                var $this = $(this);
+                var $content = $this.siblings(".more-text");
+                if($content.is(":visible")){
+                    $content.hide();
+                    $this.text("Read More");
+                } else {
+                    $content.show();
+                    $this.text("Read Less");
+                }
+            })
         })
 
 
