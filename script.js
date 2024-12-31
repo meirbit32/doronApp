9eb269fb1dd416cf1c4899bdcedf4a0861fa2504
meir biton <meirbit32@gmail.com>
2025-01-01 01:11:31 +0200

    fix menu issu add style to cards

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 
 $(document).ready(function () {
-            $(window).on("resize", function(){
-                if($(window).width() >= 764) {
-                    $(".nav-list").css("display", "flex")
-                } else{
-                    $(".nav-list").css("display", "none")
-                }
-            })
+           
             $(".material-symbols-outlined").on("click", function(event){
                 event.preventDefault()
                 $(".nav-list").toggleClass("nav-list-toogle");
